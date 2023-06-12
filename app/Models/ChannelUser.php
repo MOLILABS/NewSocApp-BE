a64d6e7411615a13b1aff8afcaa07eade6ddb503
thuan122 <127835141+thuan122@users.noreply.github.com>
2023-06-12 11:39:22 +0700

    assign channel to user

--- app/Models/ChannelUser.php
+++ app/Models/ChannelUser.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Common\Helper;
 use Illuminate\Http\Request;
+use App\Common\GlobalVariable;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ChannelUser extends BaseModel
 {
@@ -49,6 +54,37 @@ class ChannelUser extends BaseModel
             ],
             parent::getStoreValidator($request)
         );
+    }
+
+    public function storeWithCustomFormat(Request $request)
+    {
+        $global = app(GlobalVariable::class);
+        $user = $global->currentUser;
+        $abilities = User::ABILITIES;
+        $assignedUserId = $request->get('user_id');
+
+        if (Gate::allows($abilities[4])) {
+            return parent::storeWithCustomFormat($request);
+        } else if (Gate::allows($abilities[3])) {
+            // Get current user team ids
+            $teamIds = DB::table(TeamUser::retrieveTableName())
+                ->where('user_id', '=', $user->id)
+                ->pluck('team_id');
+
+            // Check if the user_id given by the current user 
+            // have the same team as the user
+            $isExist = DB::table(TeamUser::retrieveTableName())
+                ->where('user_id', '=', $assignedUserId)
+                ->whereIn('team_id', $teamIds)
+                ->exists();
+
+            if ($isExist) {
+                return parent::storeWithCustomFormat($request);
+            }
+            return Helper::getResponse('',"The team of the user is not available");
+        } else {
+            return Helper::getResponse(null, "Unauthorized", 401);
+        }
     }
 
     /**
