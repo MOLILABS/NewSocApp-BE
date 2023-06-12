06ffc360abc4994c2c0d1b02a434a70261714199
thuan122 <127835141+thuan122@users.noreply.github.com>
2023-06-12 15:12:29 +0700

    destroy assigned channel

--- app/Models/CategoryChannel.php
+++ app/Models/CategoryChannel.php
@@ -111,6 +111,63 @@ class CategoryChannel extends BaseModel
         }
     }
 
+    public function destroyWithCustomFormat($id): bool
+    {
+        $global = app(GlobalVariable::class);
+        $user = $global->currentUser;
+        $abilities = User::ABILITIES;
+
+        if (Gate::allows($abilities[11])) {
+            return parent::destroyWithCustomFormat($id);
+        } else if (Gate::allows($abilities[10])) {
+            // Get the channel id of the current record
+            $channel_id = DB::table(CategoryChannel::retrieveTableName())
+                ->where('id', '=', $id)
+                ->get('channel_id');
+
+            // Get current user's teams
+            $teamIDs = DB::table(TeamUser::retrieveTableName())
+                ->where('user_id', '=', $user->id)
+                ->pluck('team_id');
+
+            // Get all user from all the team
+            $userIds = DB::table(TeamUser::retrieveTableName())
+                ->whereIn('team_id', $teamIDs)
+                ->pluck('user_id');
+
+            // Get all channel from all the user
+            $channelIds = DB::table(ChannelUser::retrieveTableName())
+                ->whereIn('user_id', $userIds)
+                ->pluck('channel_id');
+
+            $isExist = $channelIds->contains($channel_id[0]->channel_id);
+
+            if ($isExist) {
+                return parent::destroyWithCustomFormat($id);
+            }
+            return false;
+        } else if (Gate::allows($abilities[9])) {
+            $channel_id = DB::table(CategoryChannel::retrieveTableName())
+                ->where('id', '=', $id)
+                ->get('channel_id');
+
+            // Check if the user have the channel
+            $isExist = DB::table(ChannelUser::retrieveTableName())
+                ->where('channel_id', '=', $channel_id[0]->channel_id)
+                ->where('user_id', '=', $user->id)
+                ->exists();
+
+            if($isExist)
+            {
+                return parent::destroyWithCustomFormat($id);
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
     /**
      * @return BelongsTo
      */
